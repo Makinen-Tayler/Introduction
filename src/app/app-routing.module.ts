865aaf9079c6556fce6d871ac8624c74fda042ce
Tayler <taymak@live.com>
2019-07-12 23:23:48 -0700

    First commit! Starting up the site!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+{ path: '', component: AboutComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
