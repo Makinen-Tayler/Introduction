04399c4a4f58c3921dbcd23578f1447319bede1a
Tayler <taymak@live.com>
2019-07-19 19:54:37 -0700

    Changing news

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
+  encapsulation: ViewEncapsulation.None
+
 })
+
 export class NavComponent implements OnInit {
 
   constructor() { }
